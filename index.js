3b52118d54691f32d35ad81af85f7637e3bec3d7
x414e54 <x414e54@linux.com>
2020-11-07 21:03:29 +0000

    Update index.js
    Fix missing '

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ try {
   const { exec, spawn } = require('child_process');
   
   // TODO this seems pretty insecure.
-  exec('"C:\Program Files\Epic Games\UE_4.25\Engine\Build\BatchFiles\RunUAT.bat" BuildGraph -Script="' + script + '", (err, stdout, stderr) => {
+  exec('"C:\Program Files\Epic Games\UE_4.25\Engine\Build\BatchFiles\RunUAT.bat" BuildGraph -Script="' + script + '"', (err, stdout, stderr) => {
     if (err) {
       core.setFailed(err);
       return;
