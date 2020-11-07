23b09e61ade544101506b6afa91175fb35586ed4
x414e54 <x414e54@linux.com>
2020-11-07 21:06:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const script = encodeURI(core.getInput('script'));
+  const script = escape(core.getInput('script'));
   const { exec, spawn } = require('child_process');
   
   // TODO this seems pretty insecure.
-  exec('"C:\Program Files\Epic Games\UE_4.25\Engine\Build\BatchFiles\RunUAT.bat" BuildGraph -Script="' + script + '"', (err, stdout, stderr) => {
+  exec('"C:\\Program Files\\Epic Games\\UE_4.25\\Engine\\Build\\BatchFiles\\RunUAT.bat" BuildGraph -Script="' + script + '"', (err, stdout, stderr) => {
     if (err) {
       core.setFailed(err);
       return;
