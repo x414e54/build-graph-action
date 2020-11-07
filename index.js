0c744771a0ec1db03c52ac9578351eabba703cc8
x414e54 <x414e54@linux.com>
2020-11-07 21:13:50 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const script = escape(core.getInput('script'));
+  const script = core.getInput('script');
   const { exec, spawn } = require('child_process');
+  
+  if (script.includes('\'') || script.includes('"'))
+  {
+     core.setFailed("Invalid characters in string");
+  }
   
   // TODO this seems pretty insecure.
   exec('"C:\\Program Files\\Epic Games\\UE_4.25\\Engine\\Build\\BatchFiles\\RunUAT.bat" BuildGraph -Script="' + script + '"', (err, stdout, stderr) => {
