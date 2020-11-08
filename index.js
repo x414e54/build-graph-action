e9dc9336478cca7b1e4809ec9f8182f8924067c1
x414e54 <x414e54@linux.com>
2020-11-08 13:58:01 +0000

    Update index.js
    Add output dir to script.

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ try {
   const script = core.getInput('script');
   const target = core.getInput('target');
   const projectsroot = core.getInput('projectsroot');
+  const outputdir = core.getInput('outputdir');
   const { exec, spawn } = require('child_process');
   
-  if (script.includes('\'') || script.includes('"') || target.includes('\'') || target.includes('"') || projectsroot.includes('\'') || projectsroot.includes('"'))
+  if (script.includes('\'') || script.includes('"') || target.includes('\'') || target.includes('"') || projectsroot.includes('\'') || projectsroot.includes('"') || outputdir.includes('\'') || outputdir.includes('"'))
   {
      core.setFailed("Invalid characters in string");
   }
   
   // TODO this seems pretty insecure.
-  exec('"C:\\Program Files\\Epic Games\\UE_4.25\\Engine\\Build\\BatchFiles\\RunUAT.bat" BuildGraph -Script="' + script + '" -Target="' + target + '" -set:ProjectsRoot="' + projectsroot + '"', (err, stdout, stderr) => {
+  exec('"C:\\Program Files\\Epic Games\\UE_4.25\\Engine\\Build\\BatchFiles\\RunUAT.bat" BuildGraph -Script="' + script + '" -Target="' + target + '" -set:ProjectsRoot="' + projectsroot + '" -set:OutputDir="' + outputdir + '"', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     if (err) {
