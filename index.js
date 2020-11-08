803140029d66c57f3253d54aaef2b5a943061a07
x414e54 <x414e54@linux.com>
2020-11-08 13:19:43 +0000

    Update index.js
    Add target and project root to build graph.

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const github = require('@actions/github');
 
 try {
   const script = core.getInput('script');
+  const target = core.getInput('target');
+  const projectsroot = core.getInput('projectsroot');
   const { exec, spawn } = require('child_process');
   
-  if (script.includes('\'') || script.includes('"'))
+  if (script.includes('\'') || script.includes('"') || target.includes('\'') || target.includes('"') || projectsroot.includes('\'') || projectsroot.includes('"'))
   {
      core.setFailed("Invalid characters in string");
   }
   
   // TODO this seems pretty insecure.
-  exec('"C:\\Program Files\\Epic Games\\UE_4.25\\Engine\\Build\\BatchFiles\\RunUAT.bat" BuildGraph -Script="' + script + '"', (err, stdout, stderr) => {
+  exec('"C:\\Program Files\\Epic Games\\UE_4.25\\Engine\\Build\\BatchFiles\\RunUAT.bat" BuildGraph -Script="' + script + '" -Target="' + target + '" -set:ProjectsRoot="' + projectsroot + '"', (err, stdout, stderr) => {
     console.log(stdout);
     console.log(stderr);
     if (err) {
